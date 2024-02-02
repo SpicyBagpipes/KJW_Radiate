d626f9d2f4c08b5ce0aa598404e7849b5398ce70
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2024-02-02 15:03:34 +0000

    move compats to ME

--- addons/compat_globmob/script_component.hpp
+++
@@ -1,3 +0,0 @@
-#define COMPONENT compat_globmob
-#include "\y\KJW_Radiate\addons\core\major_includes.hpp"
-#define COMPONENT_BEAUTIFIED MOD_NAME_BEAUTIFIED Compat (globmob)
--- addons/compat_m40/script_component.hpp
+++
@@ -1,3 +0,0 @@
-#define COMPONENT compat_m40
-#include "\y\KJW_Radiate\addons\core\major_includes.hpp"
-#define COMPONENT_BEAUTIFIED MOD_NAME_BEAUTIFIED Compat (M40)
