3dba6287439660c3a0a633bafb30917d4d9ba681
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2024-04-04 21:29:30 +0100

    Update ace_medical_treatment_actions.hpp
    Fixes for KAM

--- addons/core/ace_medical_treatment_actions.hpp
+++ addons/core/ace_medical_treatment_actions.hpp
@@ -2,12 +2,12 @@ class ace_medical_treatment_actions {
     class Morphine;
     class KJW_Radiate_EACA : Morphine {
         displayName = "Push EACA";
-        condition = "(([_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_hasIV) isEqualTo 1) && [_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_canAddNewMedication";
+        condition = "(([_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_hasIV) != 0) && [_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_canAddNewMedication";
         items[] = {"KJW_Radiate_EACA"};
     };
     class KJW_Radiate_EDTA : Morphine {
         displayName = "Push EDTA";
-        condition = "(([_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_hasIV) isEqualTo 1) && [_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_canAddNewMedication";
+        condition = "(([_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_hasIV) != 0) && [_patient, _bodyPart] call KJW_MedicalExpansion_Core_fnc_canAddNewMedication";
         items[] = {"KJW_Radiate_EDTA"};
     };
 };
