eab1898189b110b1ff3146fb208759bb8fad4c74
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2026-06-22 15:38:38 -0400

    Fix & Add icons

--- addons/core/CfgWeapons.hpp
+++ addons/core/CfgWeapons.hpp
@@ -5,13 +5,15 @@ class CfgWeapons {
         author = "KJW";
         descriptionShort = "A Geiger Counter used for displaying radiation counts per second.";
         displayName = "Geiger Counter";
-        picture = QPATHTOF(data\tx\geigercounter_ca.paa);
+        picture = QPATHTOF(data\tx\geigercounter_icon_ca.paa);
     };
     class ACE_morphine;
     class KJW_Radiate_EDTA : ACE_morphine {
         displayName = "EDTA";
+        picture = QPATHTOF(data\tx\edta_icon_ca.paa);
     };
     class KJW_Radiate_EACA : ACE_morphine {
         displayName = "EACA";
+        picture = QPATHTOF(data\tx\eaca_icon_ca.paa);
     };
 };
